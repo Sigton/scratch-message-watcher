dd8622e6072e6ed20b92ffb34607933615e331e4
Sigton <sigton.scratch@gmail.com>
2016-08-23 19:42:39 +0100

    Did a lot of backend work

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 import tkinter as tk
 from tkinter import ttk
-import requests
+
+try:
+    import requests
+except ImportError:
+    pass
 
 class MessageWatcherApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
 
         tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.wm_title(self, "Scratch Message Watcher")
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for f in (StartPage):
+
+            frame = f(container, self)
+
+            self.frames[f] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
 
 app = MessageWatcherApp()
 app.mainloop()
